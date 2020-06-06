326b35121f2beb5717cd493ed15297e65e16075f
Shashank <shashankpanwar4@gmail.com>
2020-06-06 07:40:26 +0530

    Initial Commit

---
+++ PhotoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by Kirti Ahlawat on 06/06/20.
+//  Copyright © 2020 Outect. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
